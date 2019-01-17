91defd12c478739452749b067d8aa131135229aa
Fanny <fanny.bertrand@gmail.com>
2019-01-17 11:41:53 +0100

    Create carte

---
+++ app/src/main/java/com/example/lucas/poke2c/database/DBManagerCarte.java
@@ -0,0 +1,65 @@
+package com.example.lucas.poke2c.database;
+
+import android.content.Context;
+
+import com.example.lucas.poke2c.model.CartePokemon;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DBManagerCarte {
+
+    private DatabaseHelper  helper;
+    private static DBManagerCarte ourInstance;
+
+    public static void init(Context context) {
+        if (ourInstance == null)
+            ourInstance = new DBManagerCarte(context);
+    }
+    public static DBManagerCarte getInstance() {
+        return ourInstance;
+    }
+
+    private DBManagerCarte(Context context) {
+        helper = new DatabaseHelper(context);
+    }
+
+    private DatabaseHelper getHelper() {
+        return helper;
+    }
+
+
+    /** Fonction pour créer une carte **/
+
+    public long createCarte(CartePokemon carte) {
+        try {
+            getHelper().getCartePokemonDao().create(carte);
+            return carte.getId();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    /** Supprimer une carte **/
+
+    public void removeCarte(CartePokemon carte) {
+        try {
+            getHelper().getCartePokemonDao().delete(carte);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public long updateCarte(CartePokemon carte) {
+        try {
+            getHelper().getCartePokemonDao().update(carte);
+            return carte.getId();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/lucas/poke2c/popup/PopupEtat.java
@@ -0,0 +1,27 @@
+package com.example.lucas.poke2c.popup;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import com.example.lucas.poke2c.R;
+
+public class PopupEtat extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popup_etat);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.6));
+
+
+    }
+}
